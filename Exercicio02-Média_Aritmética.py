07b7d99eeebda72ed7c1b339210be111ac0036dc
Le T.P <101605589+PlayerZero1@users.noreply.github.com>
2022-09-11 17:00:09 -0300

    Create Exercicio02-Média_Aritmética.py

---
+++ Exercicio02-Média_Aritmética.py
@@ -0,0 +1,12 @@
+nota01=int(input("Digite a primeira nota:"))
+nota02=int(input("Digite a segunda nota:"))
+nota03=int(input("Digite a terceira nota:"))
+nota04=int(input("Digite a quarta nota:"))
+
+
+média=(nota01+nota02+nota03+nota04)/4
+
+
+print("A média aritmética é",média)
+
+
